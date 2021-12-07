5f5a2c54df53b00a69324ab8a819cc7f1fc6dc51
Anna9067 <89978805+Anna9067@users.noreply.github.com>
2021-12-07 17:35:13 +0200

    Add files via upload

---
+++ Laboratory work 5/Task number 21/Task number 21/Task number 21.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+double factor(int k)
+{
+   double a = 1;
+   for(int i = 1; i <= k; ++i)
+   {
+       a = i * a;
+   } 
+   return a;
+}
+double numerator(int k)
+{
+    int b = -1, r;
+    double d;
+     d = k * (k - 1) / 2;
+     r = pow(b, d);
+    return r;
+}
+void sum(int n)
+{
+    double sum = 0;
+    int k = 0;
+    while (k <= n)
+    {
+        sum = numerator(k)/factor(k) + sum;
+        k = k + 1;
+    }
+    std::cout << "Sum = " << sum << "\n";
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double n, k = 0;
+    std::cout << "Введіть натуральне число: \n";
+    std::cout << "Якщо Ви хочете вийти, то натисність Ctrl+Z \n";
+    std::cout << "N = ";
+    while(std::cin >> n)
+    {
+        if ((n - (int)n) != 0)
+        {
+            std::cout << "Error: Число повинно бути натуральним! \n";
+        }
+        else if (n < 0)
+        {
+            std::cout << "Error: N мусить бути додатнім, не від'ємним! \n";
+        }
+        else
+        {
+            sum(n);
+        }
+        std::cout << "N = ";
+    } 
+    system("pause");
+    return 0;
+
+}
+
+
+
+
---
+++ Laboratory work 5/Task number 3/Task number 3/Task number 3.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+void dividers(long n)
+{
+    long d = 1;
+    int i = 0;
+    while (d <= n)
+    {
+        if (n % d == 0)
+        {
+            i = i + 1;
+            std::cout << " d [" << i << "] = " << d << "\n";
+        }
+        d = d + 1;
+    }
+}
+int main()
+{   
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    long n;
+    std::cout << " Введіть натуральне ціле число: " << "\n"; 
+    std::cout << " Якщо Ви хочете вийти, то натисність Ctrl+Z \n";
+    std::cout << "N = ";
+    while (std::cin >> n)
+    {  
+        if (n <= 0)
+        {
+            std::cout << "Error: Повинно бути додатнім!" << "\n";
+        }
+        else
+        {
+             dividers(n);
+        }
+        std::cout << "N = ";
+    } 
+    system("pause");
+    return 0;
+}
+
+
+
